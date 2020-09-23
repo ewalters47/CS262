751411d45d6f799e42066735c3349307531a663e
ewalters47 <emw45@students.calvin.edu>
2020-09-22 21:02:13 -0400

    initial commit

--- Homework1/App.js
+++ Homework1/App.js
@@ -1,23 +1,49 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard, Text } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' }
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString()},
+          ...prevTodos
+      ]
+    })
+  }
 
   return (
     <TouchableWithoutFeedback>
       <View>
         <Header />
         <View>
-          <AddTodo />
+          <AddTodo submitHandler={submitHandler} />
           <View>
-            <FlatList />
+            <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+
+            />
           </View>
         </View>
       </View>
     </TouchableWithoutFeedback>
   );
 }
+
+
--- Homework1/components/todoItem.js
+++ Homework1/components/todoItem.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function TodoItem({ pressHandler, item }) {
+export default function TodoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <View>
-        <Text>{item.text}</Text>
-      </View>
+        <Text style={styles.item}>{item.text}</Text>
     </TouchableOpacity>
   )
 };
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderRadius: 10
+    }
+})
