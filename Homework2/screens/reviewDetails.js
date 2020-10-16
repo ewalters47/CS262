e25cc07a5f668f588454aeb9a0c54301f3731256
ewalters47 <emw45@students.calvin.edu>
2020-10-16 16:11:44 -0400

    added data from monopoly dataservice

--- Homework2/screens/reviewDetails.js
+++ Homework2/screens/reviewDetails.js
@@ -1,27 +1,17 @@
-import React, { useState, useEffect} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 import Card from '../shared/card';
 
-export default function ReviewDetails({ route, navigation }) {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        fetch('https://cs262-monopoly-service.herokuapp.com/players/')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
-    }, []);
+export default function ReviewDetails({ route}) {
 
     return (
         <View style={globalStyles.container}>
             <Card>
-                <Text>{ route.params.title }</Text>
-                <Text>{ route.params.body }</Text>
-                <Text>{ route.params.rating }</Text>
+                <Text>{ route.params.id }</Text>
+                <Text>{ route.params.email }</Text>
+                <Text>{ route.params.name }</Text>
             </Card>
         </View>
     );
